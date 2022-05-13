a9bfcb2b5a2836c359787645572cb41efb85ffd0
Olfa <o.arfaoui0801@universitecentrale.tn>
2022-05-13 22:16:35 +0200

    Olfa commit

--- src/app/demande/demande.component.ts
+++ src/app/demande/demande.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-demande',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demande.component.css']
 })
 export class DemandeComponent implements OnInit {
+  readonly urlApi = "http://localhost:3000/conges";
 
-  constructor() { }
+  listConge:any;
+  constructor(private http :HttpClient) { }
 
   ngOnInit(): void {
+  }
+  addConge(val:any): Observable<any>{
+    return this.http.post(this.urlApi+"/addCongee",val);
+       
+
+}
+
+  fnValider(dateDepart:any,dateRetour:any,commentaire:any){
+    var val ={
+      dateDepart:dateDepart.value,
+      dateReoutr: dateRetour.value,
+      commentaire: commentaire.value
+    };
+
+     
+  
+  
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,18 +8,14 @@ const API_LOGIN = 'http://localhost:3000/user/login';
 @Injectable({
   providedIn: 'root'
 })
- 
-
-
 export class AuthService {
-  link='http://localhost:3000/auth/signin'
 
   constructor(
     private http: HttpClient
   ) { }
-  login(credentials: any){
-      return this.http.post(this.link ,credentials );
-    }
+  login(credentials:any ): Observable<any> {
+    return this.http.post(API_LOGIN, credentials);
+  }
   logout() {
     localStorage.removeItem('token');
   }
